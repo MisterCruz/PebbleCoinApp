cf4e26152c92ac90d1c5f88f7aada544379997a7
kushal tirumala <kushaltirumala99@gmail.com>
2015-08-16 11:54:41 -0700

    Cleanup

--- QRReaderDemo/QRReaderDemo/senddata.h
+++ QRReaderDemo/QRReaderDemo/senddata.h
@@ -3,7 +3,7 @@
 //  QRReaderDemo
 //
 //  Created by Alex Fine on 8/16/15.
-//  Copyright (c) 2015 AppCoda. All rights reserved.
+//  Copyright (c) 2015 BASE. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
